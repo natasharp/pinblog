fa2c757db36b109046d1e74b42e33bb6d5427984
natasharp <natasa_sladojevic@yahoo.com>
2021-07-19 13:30:50 +0200

    Add blog reducer

---
+++ client/src/reducers/blogReducer.js
@@ -0,0 +1,49 @@
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case 'NEW_BLOG':
+      return [...state, action.data]
+    case 'INIT_BLOGS':
+      return action.data
+    case 'UPDATE_BLOG':
+      const id = action.data.id
+      return state.map((blog) => (blog.id !== id ? blog : action.data))
+    case 'DELETE_BLOG':
+      return state.filter((blog) => blog.id !== action.data)
+    default:
+      return state
+  }
+}
+
+export const initializeBlogs = (blogs) => {
+  return async (dispatch) => {
+    dispatch({
+      type: 'INIT_BLOGS',
+      data: blogs,
+    })
+  }
+}
+
+export const addNewBlog = (newBlog) => {
+  return async (dispatch) => {
+    dispatch({
+      type: 'NEW_BLOG',
+      data: newBlog,
+    })
+  }
+}
+
+export const updateBlog = (blog) => {
+  return {
+    type: 'UPDATE_BLOG',
+    data: blog,
+  }
+}
+
+export const deleteBlog = (id) => {
+  return {
+    type: 'DELETE_BLOG',
+    data: id,
+  }
+}
+
+export default reducer
