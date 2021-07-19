9d8286f4628cfe28ed8ad0806ea6a8d737b55b59
natasharp <natasa_sladojevic@yahoo.com>
2021-07-19 12:20:21 +0200

    Refactor the app to use redux for state management

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import store from './store'
+import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, 
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root'))
---
+++ client/src/store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import blogReducer from './reducers/blogReducer'
+
+const reducer = combineReducers({
+  blogs: blogReducer
+})
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
+
+export default store
