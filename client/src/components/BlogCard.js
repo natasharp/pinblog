680d0a97f4a2212f600973d1e82cc32b954abcc5
natasharp <natasa_sladojevic@yahoo.com>
2021-08-05 17:25:24 +0200

    Refactor blog cards, and improve css

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -1,30 +1,85 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button, Card, CardContent, makeStyles } from '@material-ui/core'
-import { Link } from 'react-router-dom';
+import { Avatar, Card, CardContent, CardHeader, CardActions, IconButton, makeStyles, Paper, Box } from '@material-ui/core'
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { deleteBlog, updateBlog } from '../reducers/blogReducer'
+import blogService from '../services/blogs'
+import DeleteIcon from '@material-ui/icons/Delete';
+import FavoriteTwoToneIcon from '@material-ui/icons/FavoriteTwoTone';
+import LaunchIcon from '@material-ui/icons/Launch';
 
 const useStyle = makeStyles({
   cardStyle: {
-    minWidth: 250,
-    paddingBottom: 8
+    minWidth: 250
   },
-  cardContentStyle: {
-    paddingBottom: 4,
+  paperStyle: {
+    backgroundColor: "#ffebee"
   }
 });
 
-const BlogCard = ({ blog }) => {
+const BlogCard = ({ blog, user }) => {
   const classes = useStyle()
+  const history = useHistory()
+  const dispatch = useDispatch()
+
+  const handleDelete = async () => {
+    const message = `Remove blog ${blog.title} by ${blog.author}`
+    if (window.confirm(message)) {
+      await blogService.remove(blog.id)
+      history.push('/')
+      dispatch(deleteBlog(blog.id))
+    }
+  }
+
+  const handleLike = async () => {
+    const updatedBlog = {
+      user: blog.user.id,
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes + 1,
+      url: blog.url,
+      comments: blog.comments,
+    }
+
+    const returnedBlog = await blogService.update(blog.id, updatedBlog)
+    dispatch(updateBlog(returnedBlog))
+  }
 
   return (
     <Card className={classes.cardStyle}>
+      <Paper className={classes.paperStyle} elevation={0}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="user">
+              {blog.user.name.charAt(0)}
+            </Avatar>
+          }
+          title={blog.title}
+          subheader={`by ${blog.author}`} />
+      </Paper>
       <CardContent className={classes.cardContentStyle}>
-        <Button component={Link} to={`/blogs/${blog.id}`}>{blog.title}</Button>
-      </CardContent>
-      <CardContent className={classes.cardContentStyle}>
-        {blog.author}
-      </CardContent>
-      <CardContent className={classes.cardContentStyle}>
+        {blog.likes}
+        <Box
+          display="flex"
+          alignItems="flex-end"
+          justifyContent="flex-end">
+          <CardActions disableSpacing>
+            <IconButton aria-label="like" color="primary" onClick={() => handleLike(blog)}>
+              <FavoriteTwoToneIcon />
+            </IconButton>
+            <IconButton aria-label="launch" color="primary" component={Link} to={`/blogs/${blog.id}`}>
+              <LaunchIcon />
+            </IconButton>
+            {blog.user.username === user.username
+              ? <IconButton aria-label="delete" color="primary" onClick={() => handleDelete(blog)}>
+                <DeleteIcon />
+              </IconButton>
+              : <IconButton aria-label="delete" disabled color="primary" >
+                <DeleteIcon />
+              </IconButton>}
+          </CardActions>
+        </Box>
       </CardContent>
     </Card>
   )
