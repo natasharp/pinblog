bdf86bc4e4b5fdebb860657a8039ea3744ef0af8
natasharp <natasa_sladojevic@yahoo.com>
2021-08-17 11:30:47 +0200

    Remove unnecessary route

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,11 @@ import React, { useEffect } from 'react'
 import blogService from './services/blogs'
 import BlogForm from './components/BlogForm'
 import LoginForm from './components/LoginForm'
-import { makeStyles, Tab, Tabs, Paper, Card } from '@material-ui/core'
-import { Switch, Route, Link, useRouteMatch } from "react-router-dom"
+import { makeStyles, Tab, Tabs, Paper, Card, Container } from '@material-ui/core'
+import { Switch, Route, Link } from "react-router-dom"
 import BlogCollection from './components/BlogCollection'
 import { initializeBlogs } from './reducers/blogReducer'
 import { useDispatch, useSelector } from 'react-redux'
-import BlogDetails from './components/BlogDetails'
 import { setLoggedInUser } from './reducers/userReducer'
 import Notification from './components/Notification'
 
@@ -57,8 +56,7 @@ const App = () => {
     dispatch(setLoggedInUser(null))
     setTabValue(0)
   }
-  const match = useRouteMatch('/blogs/:id')
-  const blog = match ? blogs.find((b) => b.id === match.params.id) : null
+  
   if (!user) {
     return (
       <div>
@@ -68,7 +66,7 @@ const App = () => {
   }
 
   return (
-    <div>
+    <Container>
       <Paper className={classes.root}>
         <Tabs
           value={tabValue}
@@ -87,12 +85,11 @@ const App = () => {
           success={notification.isSuccess} /> : null}
       </Card>
       <Switch>
-        <Route path='/blogs/:id'><BlogDetails user={user} blog={blog} /></Route>
         <Route path="/login" render={() => <LoginForm />} />
         <Route path="/new" render={() => <BlogForm notification={notification} setTabValue={setTabValue} />} />
         <Route path="/collection" render={() => <BlogCollection blogs={blogs} user={user} />} />
       </Switch>
-    </div>
+    </Container>
   )
 }
 
